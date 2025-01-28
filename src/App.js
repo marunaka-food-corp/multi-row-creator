47032e08b1bbf5dbb8eb5e6b0bd4bd3bc807939e
wasabina67 <wasabina67@gmail.com>
2025-01-28 15:25:29 +0900

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>multi-row-creator</h1>
+      <h1 style={{ marginLeft: '20px' }}>multi-row-creator</h1>
+      <div
+        style={{
+          border: '2px dashed #ccc',
+          borderRadius: '4px',
+          padding: '20px',
+          textAlign: 'center'
+        }}
+      >
+        <p>xlsxファイルをここにドラッグ&ドロップして下さい。</p>
+      </div>
     </div>
   );
 }
