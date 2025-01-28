de4e35b00c200925c5da47f8e13deecdb04d4d6e
wasabina67 <wasabina67@gmail.com>
2025-01-28 18:20:26 +0900

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       };
       reader.readAsArrayBuffer(file);
     }
-  }, [selectedSheetName]);
+  }, []);
 
   const onDragOver = useCallback((e) => {
     e.preventDefault();
